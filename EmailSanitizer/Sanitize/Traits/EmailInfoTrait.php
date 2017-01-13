6df6899de7b89ea03f288215750d8e3ff5c209a4
Nikolaev Valentin <nikolaev.valentin@pdffiller.team>
2017-01-13 16:27:44 +0200

    add getSimpleEmailPart if parser failed

--- EmailSanitizer/Sanitize/Traits/EmailInfoTrait.php
+++ EmailSanitizer/Sanitize/Traits/EmailInfoTrait.php
@@ -56,11 +56,27 @@ trait EmailInfoTrait
             $result = $parser->parse($this->email);
         } catch (Exception $e) {
             $result = [
-                'local' => '',
-                'domain' => '',
+                'local'  => $this->getSimpleEmailPart($this->email, true),
+                'domain' => $this->getSimpleEmailPart($this->email),
             ];
         }
 
         return $result;
+    }
+
+    /**
+     * Get part of email address
+     *
+     * @param bool $local Local part
+     *
+     * @return string
+     */
+    protected static function getSimpleEmailPart($email, $local = false)
+    {
+        if (false === mb_strpos($email, '@')) {
+            return $email;
+        }
+
+        return ltrim(mb_strrchr($email, '@', (bool)$local), '@');
     }
 }
